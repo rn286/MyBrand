d502e52dfbb768f2b1bf4582cf8f5b264aa7e9bf
rn286 <31676112+rn286@users.noreply.github.com>
2017-09-22 13:55:40 -0400

    Update index.php
    starting over

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <?php
 
   $obj = new main();
-  $FirstName = "Rich";
-  $LastName = "Nunziante"
-  $PhoneNumber = "5551212"
-  $obj->printthis($Name);
+  $text = "my text";
+  $obj->printthis($text);
   
-    class main {
+  $array = array(1,2,3,4,5,6,7);
+  $obj->printArray($array);
+
+  class main {
 
     public function __construct() {
 
-      echo '<H1>Homework Number 3 </H1>';
-	  echo 'String Functions and Arrays<br>';
+      echo 'hello i\'m an Object </br>';
 
     }
 
-    public function printthis($Name) {
-      echo '<h2>print function and String Functions</h2>';
-      print($FirstName)<br>;
-      print($LastName)<br>;
-      echo($FirstName $LastName);
+    public function printthis($text) {
+      echo '<h1>print function demo </h1>';
+      print($text);
       echo '<hr>';
     }
 
+    public function printArray($array) {
+      echo '<h1>array print function</h1>';
+      print_r($array);
+      echo '<hr>';
+    }
+    
     public function __destruct() {
 
-      echo '</br> Assignement Completed';
+      echo '</br> I\'m Done';
 
     }
 
